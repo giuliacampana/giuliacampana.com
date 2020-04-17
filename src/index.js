1e41e763c766e9197453fdc3b6dcd94fb0078e67
Giulia Campana <giuliaecampana@gmail.com>
2020-04-17 08:45:57 -0700

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
+// @flow
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Route, Router } from 'react-router';
+import { createGlobalStyle } from 'styled-components';
+
+import AppContainer from './containers/app/AppContainer';
+import initializeRouterHistory from './core/router/RouterHistory';
 import * as serviceWorker from './serviceWorker';
+import * as Routes from './core/router/Routes';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const GlobalStyle = createGlobalStyle`
+  html,
+  body {
+    background-color: #ffffff;
+    font-family: 'Karla', sans-serif;
+    height: 100%;
+    line-height: normal;
+    margin: 0;
+    width: 100%;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+  }
+  * {
+    box-sizing: border-box;
+  }
+  *::before,
+  *::after {
+    box-sizing: border-box;
+  }
+  #root {
+    display: block;
+    height: 100%;
+    width: 100%;
+  }
+`;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+const routerHistory = initializeRouterHistory();
+
+const APP_ROOT_NODE = document.getElementById('root');
+if (APP_ROOT_NODE) {
+  ReactDOM.render(
+    <Router history={routerHistory}>
+      <Route path={Routes.ROOT} component={AppContainer} />
+      <GlobalStyle />
+    </Router>,
+    APP_ROOT_NODE
+  );
+}
+
 serviceWorker.unregister();
