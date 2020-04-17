a74272010f7ae85de6aae5582656868646461e08
Giulia Campana <giuliaecampana@gmail.com>
2020-04-17 10:43:17 -0700

    add other routes

--- src/core/router/Routes.js
+++ src/core/router/Routes.js
@@ -1,8 +1,12 @@
 // @flow
 const ROOT :string = '/';
 const PROJECTS :string = '/projects';
+const ARTS :string = '/arts';
+const ABOUT :string = '/about';
 
 export {
+  ABOUT,
+  ARTS,
   PROJECTS,
   ROOT,
 };
