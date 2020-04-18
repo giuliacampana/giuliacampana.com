b9c847d8fdea0ed076fe3c124d5486d7d0a29285
Giulia Campana <giuliaecampana@gmail.com>
2020-04-18 00:58:24 -0700

    create list of project previews in Projects screen

--- src/containers/projects/Projects.js
+++ src/containers/projects/Projects.js
@@ -3,14 +3,33 @@ import React from 'react';
 import styled from 'styled-components';
 
 import HeaderMenu from '../menu/HeaderMenu';
+import ProjectPreview from './ProjectPreview';
+import { PROJECTS } from './consts/ProjectConsts';
 
 const OuterWrapper = styled.div`
   background-color: #a484b3;
+  display: flex;
+  flex-direction: column;
+`;
+
+const InnerWrapper = styled.div`
+  background-color: #a484b3;
+  flex-direction: column;
+  margin: 20px 50px;
 `;
 
 const Projects = () => (
   <OuterWrapper>
     <HeaderMenu />
+    <InnerWrapper>
+      {
+        PROJECTS.map((project :Object) => (
+            <ProjectPreview
+                key={project.title}
+                project={project} />
+          ))
+      }
+    </InnerWrapper>
   </OuterWrapper>
 );
 
