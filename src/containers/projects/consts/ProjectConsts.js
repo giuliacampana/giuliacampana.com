1142b4fde7fe7e08170d7ad24b55eab35b8b7b43
Giulia Campana <giuliaecampana@gmail.com>
2020-04-18 00:57:40 -0700

    add projects text

---
+++ src/containers/projects/consts/ProjectConsts.js
@@ -0,0 +1,39 @@
+// @flow
+
+const CWP = {
+  blurb: `Community Work Program is an app I built at OpenLattice to help a jurisdiction
+    manage its program that prioritizes community work over jail time for low-level crimes.
+    The app tracks participation, scheduling, and eventual outcomes of people in the
+    program.`,
+  subtitle: 'Frontend React App',
+  title: 'Community Work Program',
+  years: '2019-2020',
+};
+
+const REENTRY = {
+  blurb: `Reentry Case Management is an app I built at OpenLattice for a jurisdiction
+    operating a program for people reentering society after jail or prison release.
+    The app allows users to refer participants to service providers, such as housing
+    or healthcare, that meet the needs of the person.`,
+  subtitle: 'Frontend React App',
+  title: 'Reentry Case Management',
+  years: '2020',
+};
+
+const TRAVELINN = {
+  blurb: `TravelInn is a hostel booking app with a database containing over 10 million
+    records that I optimized for minimum page load time.`,
+  subtitle: 'Full Stack Node App',
+  title: 'TravelInn',
+  years: '2018',
+};
+
+const PROJECTS = [
+  CWP,
+  REENTRY,
+  TRAVELINN,
+];
+
+export {
+  PROJECTS,
+};
