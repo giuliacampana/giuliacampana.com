8f2970d9ad216a03f399ddcc4021fbdc436ecd47
Giulia Campana <giuliaecampana@gmail.com>
2020-04-20 23:29:15 -0700

    add new routes to app container

--- src/containers/app/AppContainer.js
+++ src/containers/app/AppContainer.js
@@ -4,11 +4,15 @@ import { Route, Switch } from 'react-router';
 
 import IntroPage from '../intro/IntroPage';
 import Projects from '../projects/Projects';
+import Arts from '../arts/Arts';
+import About from '../about/About';
 import * as Routes from '../../core/router/Routes';
 
 const AppContainer = () => {
   return (
     <Switch>
+      <Route path={Routes.ABOUT} component={About} />
+      <Route path={Routes.ARTS} component={Arts} />
       <Route path={Routes.PROJECTS} component={Projects} />
       <Route path={Routes.ROOT} component={IntroPage} />
     </Switch>
